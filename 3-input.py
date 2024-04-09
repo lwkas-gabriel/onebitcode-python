295ecfb13e509050b5bdd250a14b0ec626e9cc76
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-04-09 19:32:26 -0300

    entrada de dados

---
+++ 3-input.py
@@ -0,0 +1,9 @@
+name = input("Digite o nome do jogo: \n")
+yearLaunch = int(input("Digite o ano de lançamento do jogo: \n"));
+price = float(input("Digite o preço do jogo: \n"));
+planIncluded = bool(input("Está incluso no serviço mensal?: \n"));
+
+print(name);
+print(yearLaunch);
+print(price);
+print(planIncluded);
