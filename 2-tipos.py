f63c1b664b88005b85cc59f5d8517c4bcdf21dee
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-04-09 19:14:34 -0300

    mudando estrutura e tipos de dados

---
+++ 2-tipos.py
@@ -0,0 +1,7 @@
+name = "Resident Evil 4 Remake"
+yearLaunch = 2023;
+price = 300.00;
+planIncluded = False;
+
+print(name, yearLaunch, price, planIncluded); #concatenando valores
+print(type(name), type(yearLaunch), type(price), type(planIncluded)); #concatenando valores, de novo
