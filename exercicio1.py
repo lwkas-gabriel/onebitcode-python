1237a1f7171ee615d006e34015de9a5564646730
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-04-09 20:03:52 -0300

    refatorando a primeira solução

--- exercicio1.py
+++ exercicio1.py
@@ -1,18 +1,16 @@
 ## Escreva um programa em python que leia um numero e represente o seu antecessor e sucessor, utilziando operadores de atribuição.
 
 number = int(input("Inserir número para descobrir antecessor e sucessor dele: \n"));
-sucessor = number + 1;
-antecessor = number - 1;
-print(f"o sucessor de {number} é {sucessor} e o seu antecessor é {antecessor}");
+print(f"o sucessor de {number} é {number+1} e o seu antecessor é {number-1}");
 
 
 ## Escreva um programa que calcule a média de quatro notas
 
-nota = 0;
+nota = 0.0;
 nota += float(input("Insira a primeira nota: \n"));
 nota += float(input("Insira a segunda nota: \n"));
 nota += float(input("Insira a terceira nota: \n"));
-nota += float(input("Insira a quarta nota: \n"));
+nota += float(input("Insira a quarta nota: \n")); 
 
 media = nota/4
 
