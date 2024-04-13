32ca22d0fdfc9a760f6de76ea876e836efb39df4
lwkas-gabriel <lwkasgabriel@hotmail.com>
2024-04-13 15:26:00 -0300

    operações com strings

---
+++ 7-operacoes-string.py
@@ -0,0 +1,21 @@
+gameDescription = """
+    Fifa 23 é um jogo de futebol
+    desenvolvido pela EA Sports
+    e que possibilita jogar localmente ou online
+"""; ##estrutura de comentário multi-linha
+
+line = "="
+gameName = "Resident Evil";
+gameVersion = " 8";
+
+# 1- operação de concatenação de strings
+print(line * 25);
+gameFullName = gameName + gameVersion;
+print(gameFullName);
+
+#2 - operacao de multiplcacao de strings
+print(line * 25);
+
+#3 - procarar palavra dentro de um texto
+print("Fifa" in gameDescription);
+print("futebol" in gameDescription);
